0b8c45f0db6f5e4c971224981bf1e85a2bd6e3dd
“elif-cotton” <elif.watercolors@gmail.com>
2024-05-01 11:52:58 -0400

    Agregando card

--- js/script.js
+++ js/script.js
@@ -16,13 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
+// Funciones de los botones del navegador
 document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll('.header-productos button');
     const blocks = document.querySelectorAll('.contenido-productos .content_text');
 
     buttons.forEach((button, index) => {
         button.addEventListener('click', () => {
+            // Quitar la clase "marked" de todos los botones
+            buttons.forEach(btn => btn.classList.remove('marked'));
+            // Agregar la clase "marked" solo al botón clickeado
+            button.classList.add('marked');
+
             // Oculta todos los bloques de contenido
             blocks.forEach(block => {
                 block.classList.add('hidden');

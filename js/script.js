24c3ed1746e915170dd2e65e3624a9ecd120805c
elxrojo <elxrojo115@gmail.com>
2024-05-18 20:14:48 -0300

    finishing responsive

--- js/script.js
+++ js/script.js
@@ -34,8 +34,8 @@ $(document).ready(function () {
     $('#asddas').slick({
         infinite: true,
         dots: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         arrows: false,
     });
 });
